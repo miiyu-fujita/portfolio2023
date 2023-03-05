dee6e64975f609502629fd2f64571a4ceafcc907
Miiyu <71932936+miiyu-fujita@users.noreply.github.com>
2023-03-05 17:36:29 -0500

    Add background color & parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
+import LoadingPage from './components/loadingPage/LoadingPage';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background background-color">
+      <Parallax pages={5}>
+        <ParallaxLayer></ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
